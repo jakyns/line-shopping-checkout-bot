eb9d532fd7a95035d105494fbc83544d99a850c2
bank <jak.yns@gmail.com>
2022-02-14 10:11:07 +0700

    Change timeout from 2s to 1s

--- tests/checkout-babylovett.spec.ts
+++ tests/checkout-babylovett.spec.ts
@@ -15,15 +15,14 @@ test.describe('scenarios#1', async () => {
 
     await Promise.all([
       page.waitForNavigation(/*{ url: 'https://shop.line.me/@babylovett/checkout/2021113015181263/' }*/),
-      page.click('xpath=//*[@id="0"]/div[4]/div/div/div[2]/button', { timeout: 2000 })
+      page.click('xpath=//*[@id="0"]/div[4]/div/div/div[2]/button', { timeout: 1000 })
     ]);
 
     await page.click('.absolute.top-20xem');
 
     await page.click('text=Shipping Address Manage Address อภิญญา แซ่หว่อง 293/45 หมู่บ้าน Perfect Park เฟส >> svg');
-    await page.click('text=Kerry ฿40.00 Thailand Post-EMS กทม. 1-2 วันทำการ, ตจว. 2-3 วันทำการ ฿40.00 >> svg');
-    await page.click('text=Pay with Rabbit LINE Pay Check out and earn points Pay with a credit/debit card  >> svg');
-
+    await page.click('text=Kerry ฿40.00 Thailand Post-EMS กทม. 1-2 วันทำการ, ตจว. 2-3 วันทำการ ฿35.00 >> svg');
+    await page.click('text=Pay with Rabbit LINE Pay Pay with a credit/debit card Powered by >> svg');
 
     await Promise.all([
       page.waitForNavigation(/*{ url: 'https://shop.line.me/@babylovett/checkout/2021113015181263/bank-inform' }*/),
--- tests/checkout-tillymilly.spec.ts
+++ tests/checkout-tillymilly.spec.ts
@@ -15,7 +15,7 @@ test.describe('scenarios#1', async () => {
 
     await Promise.all([
       page.waitForNavigation(/*{ url: 'https://shop.line.me/@tillymilly/checkout/2021121816240500/' }*/),
-      page.click('xpath=//*[@id="0"]/div[4]/div/div/div[2]/button', { timeout: 2000 })
+      page.click('xpath=//*[@id="0"]/div[4]/div/div/div[2]/button', { timeout: 1000 })
     ]);
 
     await page.click('.absolute.top-20xem');
